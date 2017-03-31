66197549d7375b2a9074fe8da3d746c512380bc6
Haoran Feng <haoran.feng@alo7.com>
2017-03-31 18:42:23 +0800

    init state

---
+++ 06/main.js
@@ -0,0 +1,17 @@
+var Redux = require('redux');
+
+const counter = (state  = 0, action) =>{
+  switch(action.type) {
+  case 'INCREMENT':
+    return state + 1;
+  case 'DECREMENT':
+    return state - 1;
+  default:
+      return state;
+  }
+}
+
+const { createStore } = Redux;
+const  store = createStore(counter);
+
+console.log(store.getState());
