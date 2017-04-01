a5948960b19488c564bd49b7849619e83b1341d5
Haoran Feng <haoran.feng@alo7.com>
2017-04-01 09:33:03 +0800

    remove test case

--- 09/main.js
+++ 09/main.js
@@ -4,6 +4,12 @@ const addCounter = (list) => {
   return [...list, 0];          // es6 style
 }
 
+const removeCounter = (list, index) => {
+  return [
+    ...list.slice(0, index),
+    ...list.slice(index+1)
+  ];
+}
 const testAddCounter = () => {
   const listBefore = [];
   const listAfter = [0];
@@ -14,5 +20,17 @@ const testAddCounter = () => {
   ).toEqual(listAfter);
 };
 
+const testRemoveCounter = () => {
+  const listBefore = [0, 10, 20];
+  const listAfter = [0, 20];
+
+  deepFreeze(listBefore);
+  expect(
+    removeCounter(listBefore, 1)
+  ).toEqual(listAfter);
+};
+
+
 testAddCounter();
+testRemoveCounter();
 console.log('All Tests passed');
