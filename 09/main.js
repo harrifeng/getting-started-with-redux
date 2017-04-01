4a288a38c50f080ef064c94e67b8d23b9cdc4edf
Haoran Feng <haoran.feng@alo7.com>
2017-04-01 09:36:04 +0800

    last test case

--- 09/main.js
+++ 09/main.js
@@ -10,6 +10,15 @@ const removeCounter = (list, index) => {
     ...list.slice(index+1)
   ];
 }
+
+const incrementCounter = (list, index) => {
+  return [
+    ...list.slice(0, index),
+    list[index] + 1,
+    ...list.slice(index+1)
+  ];
+}
+
 const testAddCounter = () => {
   const listBefore = [];
   const listAfter = [0];
@@ -30,7 +39,17 @@ const testRemoveCounter = () => {
   ).toEqual(listAfter);
 };
 
+const testIncrementCounter = () => {
+  const listBefore = [0, 10, 20];
+  const listAfter = [0, 11, 20];
+
+  deepFreeze(listBefore);
+  expect(
+    incrementCounter(listBefore, 1)
+  ).toEqual(listAfter);
+};
 
 testAddCounter();
 testRemoveCounter();
+testIncrementCounter();
 console.log('All Tests passed');
